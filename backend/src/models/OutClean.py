5a4443192f50908273cbe25d34feac74faf2a21a
Minerva Ciccarese <minervaciccarese@outlook.com>
2024-10-14 11:54:08 +0300

    #5 Added function that loads outdoor gym data files in their own directory. Currently if the OutClean function is run after loading the files an error will be raised ([Errno 2] No such file or directory:). Error will be fixed asap.

--- backend/src/models/OutClean.py
+++ backend/src/models/OutClean.py
@@ -1,14 +1,41 @@
+import requests
+from bs4 import BeautifulSoup
+import os
 import pandas as pd
+
+# Code to load the compressed .csv files:
+
+url = "https://hkikanslialiikuntapaikat.z6.web.core.windows.net/ulkokuntosali/index.html" # Source url for outdoor gym usage data
+os.makedirs("outfiles", exist_ok=True) # Directory to store outdoor the files
+
+def loadcsv(): # Get the needed .csv files from the source (as we need .csv files and the source has both .csv and .json)
+    response = requests.get(url)
+    soup = BeautifulSoup(response.text, 'html.parser')
+    for link in soup.find_all('a'): # Find all links in source page
+        href = link.get('href')
+        if href and href.endswith('.csv.gz') and 'hourly' in href: # Note: the files are all compressed, hence why the href needs to end in '.csv.gz'. Also 'hourly' must be in the href, as we want hourly data files
+            if '2023' in href or '2024' in href: # We want data from 2023 and 2024, hence why the href must contain either one of those strings
+                loadurl = 'https://hkikanslialiikuntapaikat.z6.web.core.windows.net/ulkokuntosali/' + href  # Note: url to load files is different than source page url! 
+                print(f"Downloading {loadurl}...")
+                csvres = requests.get(loadurl)
+                csvres.raise_for_status()
+                csvfile = os.path.join("outfiles", os.path.basename(href)) # .csv gets loaded in the outfiles directory
+                with open(csvfile, 'wb') as f:
+                    f.write(csvres.content)
+                print(f"Saved {csvfile}") # Prints if loading was successful
+
+# Code to clean the previously-loaded files:
+
 def OutClean(file): # Remove needless columns and cleans up data for processing
     remove = ['groupId', 'trackableId', 'sets', 'repetitions'] # Needless columns
-    data = pd.read_csv(file) # Read file
+    file = pd.read_csv(file, compression = 'gzip') # Unzip and read files
     for val in remove:
-        data = data.drop(val, axis = 1) # Removes columns in remove list
-    data[['day','hour']] = data.utctimestamp.apply(lambda x: pd.Series(str(x).split("T"))) # Splits utctimestamp column in date and time columns. Column names are same to UniSport date and time columns
-    data = data.drop('utctimestamp', axis = 1) # Remove utctimestamp column
-    data.insert(0, 'day', data.pop('day')) # Set day as first column
-    data.insert(1, 'hour', data.pop('hour')) # Set hour as second column
-    data['day'] = pd.to_datetime(data['day']).dt.strftime('%d/%m/%Y') # Set day column values to match format of UniSport day column
-    data['hour'] = pd.to_numeric(data['hour'].str[:2]) # Keep only first two digit of hour column (the actual hour) and turn the value to integer type
-    data = data.groupby(['day', 'hour', 'area'])['usageMinutes'].sum() # Sum usageMinutes cells in rows with the same day, hour and area
-    return data
+        file = file.drop(val, axis = 1) # Removes columns in remove list
+    file[['day','hour']] = file.utctimestamp.apply(lambda x: pd.Series(str(x).split("T"))) # Splits utctimestamp column in date and time columns. Column names are same to UniSport date and time columns
+    file = file.drop('utctimestamp', axis = 1) # Remove utctimestamp column
+    file.insert(0, 'day', file.pop('day')) # Set day as first column
+    file.insert(1, 'hour', file.pop('hour')) # Set hour as second column
+    file['day'] = pd.to_datetime(file['day']).dt.strftime('%d/%m/%Y') # Set day column values to match format of UniSport day column
+    file['hour'] = pd.to_numeric(file['hour'].str[:2]) # Keep only first two digit of hour column (the actual hour) and turn the value to integer type
+    file = file.groupby(['day', 'hour', 'area'])['usageMinutes'].sum() # Sum usageMinutes cells in rows with the same day, hour and area
+    return file
